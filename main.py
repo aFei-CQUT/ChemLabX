e3230ffd378d9001d1e761e8ac843147e765925f
LiuKangFei <2039787966@qq.com>
2025-04-26 01:09:55 +0800

    格式化

--- main.py
+++ main.py
@@ -55,14 +55,16 @@ time_upper_limit = 40  # 自动寻找平台期的最大时间窗口
 width_height_inches = (10, 6)  # 保存图片尺寸，单位英尺
 dpi = 600  # 保存图片DPI
 
-def clear_pycache(root_dir='.'):
+
+def clear_pycache(root_dir="."):
     for dirpath, dirnames, _ in os.walk(root_dir):
         for dirname in dirnames:
             # 如果是 __pycache__ 文件夹，删除它
-            if dirname == '__pycache__':
+            if dirname == "__pycache__":
                 dir_to_delete = os.path.join(dirpath, dirname)
-                print(f"Deleting: {dir_to_delete}")
+                # print(f"Deleting: {dir_to_delete}")
                 shutil.rmtree(dir_to_delete)
+
 
 if __name__ == "__main__":
 
